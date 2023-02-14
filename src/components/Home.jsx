c75b8bd0ca7faf555a07227ccf51b5cbe665fdb3
Kishan <bang.siki2009@gmail.com>
2023-02-14 13:05:30 +0545

    Change bg image loading method

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
     return (
-        <div id="main" className="bg-hero bg-no-repeat bg-cover bg-center bg-fixed">
+        <div id="main" className="bg-[url('https://images.unsplash.com/photo-1473830394358-91588751b241?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')] bg-no-repeat bg-cover bg-center bg-fixed">
             <div className="flex flex-col justify-start items-center space-y-6 w-full h-screen text-white bg-black/50">
                 <p className="text-3xl md:text-4xl mt-80 mb-4">Welcome</p>
                 <TypeAnimation
